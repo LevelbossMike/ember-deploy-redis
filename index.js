37ab3a91744e65f39afdca23432736dab02e6f0f
Michael Klein <michael@firstiwaslike.com>
2015-01-06 21:49:54 +0100

    Finish moving redis adapter from ember-deploy.

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 /* jshint node: true */
 'use strict';
 
+var RedisAdapter = require('./lib/redis');
+
 module.exports = {
-  name: 'ember-deploy-redis'
+  name: 'ember-deploy-redis',
+  type: 'ember-deploy-addon',
+
+  adapters: {
+    index: {
+      'redis': RedisAdapter
+    }
+  }
 };
